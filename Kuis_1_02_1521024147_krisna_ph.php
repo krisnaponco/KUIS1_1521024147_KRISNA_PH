fff463c35a53b0f4306a5d558fd8f082cf3cd279
krisnapanca333@gmail.com <sumuragung333>
2016-11-22 20:23:51 +0700

    KUIS KRISNA

---
+++ Kuis_1_02_1521024147_krisna_ph.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>AKN Bojonegoro</title>
+</head>
+<body>
+<?php
+$kalimat = array
+$kalimat = (array("krisna 1521024147 ph");
+echo $kalimat[0][0]. .$kalimat[0][1]. .$kalimat[0][2].;
+?>
+</body>
+</html>
